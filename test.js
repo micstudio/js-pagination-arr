724c8ecb42ac89e1bb7eaf119b25e9993b75cd57
mic <michal.bukarowicz@gmail.com>
2020-05-02 18:14:59 +0200

    v2

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const paginateArray = (dataEntries, actualPageIdx, entriesOnPage) => {
 
-    
     if(actualPageIdx === 1 ) {
         let arr2 = dataEntries.slice(0,entriesOnPage)
         return console.log(arr2);
@@ -12,15 +11,12 @@ const paginateArray = (dataEntries, actualPageIdx, entriesOnPage) => {
    }
 
 
+    let l1 = sum-entriesOnPage
 
-    let l1 = dataEntries[sum]-entriesOnPage-1
-    let l2 = dataEntries[sum]-1
-
-    let arr3 = dataEntries.slice(l1,l2)
+    let entriesOnSelectedPage = dataEntries.slice(l1,sum)
    
-    console.log(l1,l2);
-    console.log(arr3);
+    console.log(entriesOnSelectedPage);
+
+    return entriesOnSelectedPage
 }
-arr = [1,2,3,4,5,6,7,8,9,10,11,12,10,11,12,10,11,12,10,11,12,13,14,15,16,17,18,19,20]
-paginateArray(arr,2,41) 
 
